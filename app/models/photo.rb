18f4e1c41b9444d85e833e393753062c16e9ee1a
Javier Cruz <jcruzsm@gmail.com>
2015-04-23 10:45:51 -0300

    updated info S3 of AWS. Access key, secret key and bucket

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,14 @@ class Photo < ActiveRecord::Base
   belongs_to :gabinete_object
   validates :gabinete_object, presence: true
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, 'a imagem tem que ser menor que 5MB')
+      end
+    end
+
 end
