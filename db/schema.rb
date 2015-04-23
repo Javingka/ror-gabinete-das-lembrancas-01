74f899030d40872dfc46a4626a18c1a25f79ecb9
Javier Cruz <jcruzsm@gmail.com>
2015-04-23 12:21:00 -0300

    updated migration creation for photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422205113) do
+ActiveRecord::Schema.define(version: 20150423151237) do
 
   create_table "exhibits", force: :cascade do |t|
     t.string   "cidade"
