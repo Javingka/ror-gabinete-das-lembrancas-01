0f0c11ead6261a07b46bb2129d3806dfcf72bb71
Javier Cruz <jcruzsm@gmail.com>
2015-04-23 12:14:04 -0300

    updated migration creation for photos

--- db/migrate/20150422205056_create_photos.rb
+++
@@ -1,8 +0,0 @@
-class CreatePhotos < ActiveRecord::Migration
-  def change
-    create_table :photos do |t|
-      t.string :picture
-      t.references :gabinete_object, index: true, foreign_key: true
-    end
-  end
-end
