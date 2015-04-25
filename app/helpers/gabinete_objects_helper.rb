c571f842f79253141626ac6f6c7f26c72de7ca7e
Javier Cruz <jcruzsm@gmail.com>
2015-04-25 16:02:09 -0300

    pagina de visualização de objetos pronta

--- app/helpers/gabinete_objects_helper.rb
+++ app/helpers/gabinete_objects_helper.rb
@@ -3,7 +3,21 @@ module GabineteObjectsHelper
   def set_actual_obj(obj, expo)
     @obj = obj
     @exp_obj = expo
+    next_o = false 
+    @obj_n = nil 
+    @exp_obj.gabinete_objects.each_with_index do |o,i| 
+      puts 'prints:  '+@gabinete_object.id.to_s+' obj o: '+o.id.to_s
+      if next_o 
+        @obj_n = o 
+        break 
+      end 
+      if o.id == @gabinete_object.id 
+        next_o = true 
+        @obj_n = @exp_obj.gabinete_objects.first if i == @exp_obj.gabinete_objects.size-1
+      end 
+    end
   end
+  
   def get_actual_obj
     @obj
   end
