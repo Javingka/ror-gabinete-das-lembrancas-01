68b72bb8541cf64637ccd3826a78d9f3b5072436
Javier Cruz <jcruzsm@gmail.com>
2015-08-11 11:44:09 -0300

    pagina /acbeu com lista dos Objetos da turne Chapada

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,7 +10,16 @@ class StaticPagesController < ApplicationController
 
   def acbeu
     @gabinete_objects = GabineteObject.all
-    
+    @gabinete_objects_chapada = Array.new
+    @gabinete_objects.each do |o|
+      expo = Exhibit.find_by(id: o.exhibit_id) 
+      if expo.cidade != "Rio de Contas" 
+        @gabinete_objects_chapada << o
+      end
+    end
+
+    puts @gabinete_objects_chapada
+
     tempo = "01:00" # TODO pegar o tempo desde GabineteObj... 
     src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/111545654' # TODO pegar a scr desde GabineteObj...
     @src_audio = src+'#t='+tempo.to_s;  
