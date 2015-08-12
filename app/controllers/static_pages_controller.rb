38114a4deb5af14004cf0e7d773417812ddcf83b
Javier Cruz <jcruzsm@gmail.com>
2015-08-11 22:07:03 -0300

    numero ID de objetos em ordem na exhibição

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,14 +11,17 @@ class StaticPagesController < ApplicationController
   def acbeu
     @gabinete_objects = GabineteObject.all
     @gabinete_objects_chapada = Array.new
+
+
+
     @gabinete_objects.each do |o|
       expo = Exhibit.find_by(id: o.exhibit_id) 
-      if expo.cidade != "Rio de Contas" 
+      if expo.cidade != "Rio de Contas" && !o.expo_ID.blank?
         @gabinete_objects_chapada << o
       end
     end
-
-    puts @gabinete_objects_chapada
+    @gabinete_objects_chapada = @gabinete_objects_chapada.sort_by { |hsh| hsh.expo_ID }
+    puts @gabinete_objects_chapada.to_s
 
     tempo = "01:00" # TODO pegar o tempo desde GabineteObj... 
     src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/111545654' # TODO pegar a scr desde GabineteObj...
