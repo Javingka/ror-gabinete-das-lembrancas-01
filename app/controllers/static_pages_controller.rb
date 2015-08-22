cbcfe05ae6961c64358933a754aa88429f3d7f96
Javier Cruz <jcruzsm@gmail.com>
2015-08-22 14:22:49 -0300

    nova interfaz sonora

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,12 +18,12 @@ class StaticPagesController < ApplicationController
       expo = Exhibit.find_by(id: o.exhibit_id) 
       if expo.cidade != "Rio de Contas" && !o.expo_ID.blank?
         if o.audio_link.blank?
-          o.audio_link = "http://www.noiseaddicts.com/samples_1w72b820/4939.mp3"
+          o.audio_link = "http://www.noiseaddicts.com/samples_1w72b820/4939.mp3" #Coloca o som de burro quando não tem som
         end
         @gabinete_objects_chapada << o
       end
     end
-    @gabinete_objects_chapada = @gabinete_objects_chapada.sort_by { |hsh| hsh.expo_ID }
+    @gabinete_objects_chapada = @gabinete_objects_chapada.sort_by { |hsh| hsh.expo_ID } #Objetos ordenados segundo expo
     puts @gabinete_objects_chapada.to_s
 
     tempo = "01:00" # TODO pegar o tempo desde GabineteObj... 
