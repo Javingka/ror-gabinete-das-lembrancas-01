6b880f6f9bf5dc2d24e624228e9a0f6ec1f89c75
Javier Cruz <jcruzsm@gmail.com>
2015-10-02 12:05:44 -0300

    melhoras menores no display

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -15,7 +15,7 @@ class StaticPagesController < ApplicationController
 
 
     @gabinete_objects.each do |o|
-      expo = Exhibit.find_by(id: o.exhibit_id) 
+      expo = Exhibit.find_by(id: o.exhibit_id)
       if expo.cidade != "Rio de Contas" && !o.expo_ID.blank?
         if o.audio_link.blank?
           o.audio_link = "http://www.noiseaddicts.com/samples_1w72b820/4939.mp3" #Coloca o som de burro quando não tem som
@@ -26,9 +26,9 @@ class StaticPagesController < ApplicationController
     @gabinete_objects_chapada = @gabinete_objects_chapada.sort_by { |hsh| hsh.expo_ID } #Objetos ordenados segundo expo
     puts @gabinete_objects_chapada.to_s
 
-    tempo = "01:00" # TODO pegar o tempo desde GabineteObj... 
+    tempo = "01:00" # TODO pegar o tempo desde GabineteObj...
     src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/111545654' # TODO pegar a scr desde GabineteObj...
-    @src_audio = src+'#t='+tempo.to_s;  
+    @src_audio = src+'#t='+tempo.to_s;
   end
 
 end
